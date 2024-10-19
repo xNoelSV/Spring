a3e79aa92f03259e091f2a2fc7432fa29ba61bf1
Noel Sariñena Varela <noelsava25@gmail.com>
2024-10-19 20:48:31 +0200

    pUSH

--- springboot-web/src/main/java/com/noel/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ springboot-web/src/main/java/com/noel/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.noel.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.noel.curso.springboot.webapp.springboot_web.models.User;
@@ -14,10 +18,26 @@ public class UserController {
 
     User user = new User("Noel", "Sariñena");
 
+    user.setEmail("noelsava25@gmail.com");
     model.addAttribute("title", "Hola Mundo Spring Boot");
     model.addAttribute("user", user);
-    
+
     return "details";
+  }
+
+  @GetMapping("/list")
+  public String list(ModelMap model) {
+    List<User> users = Arrays.asList(
+      new User("Pepa", "Gonzalez"), 
+      new User("Lalo", "Perez", "lalo@correo.com"),
+      new User("Juanita", "Roe", "juana@correo.com"),
+      new User("Noel", "Sariñena")
+    );
+
+    model.addAttribute("users", users);
+    model.addAttribute("title", "Listado de usuarios");
+
+    return "list";
   }
 
 }
--- springboot-web/src/main/java/com/noel/curso/springboot/webapp/springboot_web/models/User.java
+++ springboot-web/src/main/java/com/noel/curso/springboot/webapp/springboot_web/models/User.java
@@ -5,6 +5,7 @@ public class User {
   // ATRIBUTOS
   private String name;
   private String lastname;
+  private String email;
 
   // CONSTRUCTORES
   public User() {
@@ -15,20 +16,34 @@ public class User {
     this.lastname = lastname;
   }
 
+  public User(String name, String lastname, String email) {
+    this(name, lastname);
+    this.email = email;
+  }
+
   // GETTERS Y SETTERS
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
+
   public String getLastname() {
     return lastname;
   }
+
   public void setLastname(String lastname) {
     this.lastname = lastname;
   }
 
-  
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
 }
