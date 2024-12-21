4a8c2568a1473d75e2e09d496b57655089a6cbeb
Noel Sariñena Varela <noelsava25@gmail.com>
2024-12-21 18:55:42 +0100

    Push Laptop

---
+++ Spring_microservices/section2/accounts/src/main/java/com/noels/accounts/dto/AccountsDto.java
@@ -0,0 +1,14 @@
+package com.noels.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    private Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+
+}
