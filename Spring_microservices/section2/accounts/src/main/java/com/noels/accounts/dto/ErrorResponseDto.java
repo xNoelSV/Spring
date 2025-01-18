633663f7449c42651b8fd27fb3acc50c051d203c
Noel Sariñena Varela <noelsava25@gmail.com>
2025-01-18 18:34:26 +0100

    Push

--- Spring_microservices/section2/accounts/src/main/java/com/noels/accounts/dto/ErrorResponseDto.java
+++ Spring_microservices/section2/accounts/src/main/java/com/noels/accounts/dto/ErrorResponseDto.java
@@ -2,14 +2,15 @@ package com.noels.accounts.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponseDto {
-
     private String apiPath;
     private HttpStatus errorCode;
     private String errorMessage;
--- Spring_microservices/section2/accounts/src/main/java/com/noels/accounts/dto/ResponseDto.java
+++ Spring_microservices/section2/accounts/src/main/java/com/noels/accounts/dto/ResponseDto.java
@@ -2,8 +2,11 @@ package com.noels.accounts.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data @AllArgsConstructor
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseDto {
 
     private String statusCode;
