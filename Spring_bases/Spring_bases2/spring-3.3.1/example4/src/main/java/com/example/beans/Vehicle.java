3aa20f7b308454e21808e7de1176838ca4c98fb1
Noel Sariñena Varela <noelsava25@gmail.com>
2025-01-19 21:24:00 +0100

    Push

---
+++ Spring_bases/Spring_bases2/spring-3.3.1/example4/src/main/java/com/example/beans/Vehicle.java
@@ -0,0 +1,17 @@
+package com.example.beans;
+
+/**
+ *  Vehicle Class
+ */
+public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
