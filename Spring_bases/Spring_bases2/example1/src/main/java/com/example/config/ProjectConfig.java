bf50aa7757ed4b951ce37f205baa0030b9f4f427
Noel Sariñena Varela <noelsava25@gmail.com>
2025-01-20 16:46:31 +0100

    Push

---
+++ Spring_bases/Spring_bases2/example1/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,48 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This configuration class is used to define the beans for the project.
+ */
+@Configuration
+public class ProjectConfig {
+
+    /**
+     * Creates and returns a Vehicle bean instance.
+     *
+     * This method configures a Vehicle object, sets its name to "Audi 8",
+     * and registers it as a Spring bean in the application context.
+     *
+     * @return a configured Vehicle instance.
+     */
+    @Bean
+    Vehicle vehicle() {
+        var veh = new Vehicle();
+        veh.setName("Audi 8");
+        return veh;
+    }
+
+    /**
+     * Returns a simple hello world message.
+     *
+     * @return a hello world message.
+     */
+    @Bean
+    String helloI () {
+        return "Hello World";
+    }
+
+    /**
+     * Returns a predefined integer value.
+     *
+     * @return an integer value.
+     */
+    @Bean
+    Integer number () {
+        return 16;
+    }
+
+}
