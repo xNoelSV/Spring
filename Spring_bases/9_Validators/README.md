51fe289db36da56063ead2121764de9dce618cb8
xNoelSV <noelsava25@gmail.com>
2025-05-30 21:15:39 +0200

    Push

---
+++ Spring_bases/9_Validators/README.md
@@ -0,0 +1,10 @@
+### Contents of this section
+<br>
+
+Server side validations because client side validations can be bypassed.
+Jakarta Bean Validation:
+- @NotNull
+- @Positive
+- @Email
+- @Valid
+- ...
---
+++ Spring_bases/9_Validators/example24/src/main/java/com/xnoelsv/school/model/Holiday.java
@@ -0,0 +1,16 @@
+package com.xnoelsv.school.model;
+
+import lombok.Data;
+
+@Data
+public class Holiday {
+
+    private final String day;
+    private final String reason;
+    private final Type type;
+
+    public enum Type {
+        FESTIVAL, FEDERAL
+    }
+
+}
