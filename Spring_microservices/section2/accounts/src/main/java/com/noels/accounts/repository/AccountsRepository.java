1246747dc6314e970fc5eda569d4de4a4a283461
xNoelSV <noelsava25@gmail.com>
2025-07-14 15:59:53 +0200

    Push

--- Spring_microservices/section2/accounts/src/main/java/com/noels/accounts/repository/AccountsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.noels.accounts.repository;
-
-import com.noels.accounts.entity.Accounts;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountsRepository extends JpaRepository<Accounts, Long> {
-}
